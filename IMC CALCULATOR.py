73d9ede913603a12425a1b98f7764f7420a79a79
DOPEKILL431 <you@example.com>
2022-06-23 15:49:58 +0000

    ajout diagnostic

--- IMC CALCULATOR.py
+++ IMC CALCULATOR.py
@@ -1,4 +1,4 @@
-#IMC = poids en kg/taille² (en m)
+
 
 from fileinput import close
 from http.client import OK
@@ -15,12 +15,27 @@ def go():
     FPOIDS=EPOIDS.get()
     FTAILLE=ETAILLE.get()
     res=eval(FPOIDS)/eval((FTAILLE))**2
-    RESULTAT.insert(0,res)
+    RESULTAT.insert(0,res.__round__(1))
+    if res<18.5:
+        DIAGNOSTIC.insert(0,"Dénutrition")
+    elif res>=18.5 and res<25:
+        DIAGNOSTIC.insert(0,"Normal")
+    elif res>=25 and res<30:
+        DIAGNOSTIC.insert(0,"Surpoids")
+    elif res>=30 and res<35:
+        DIAGNOSTIC.insert(0,"Obésité modérée")
+    elif res>=35 and res<40:
+        DIAGNOSTIC.insert(0,"Obésité sévère")
+    elif res>=40:
+        DIAGNOSTIC.insert(0,"Obésité morbide")
+
+
 
 def reset():
     EPOIDS.delete(0,END)
     ETAILLE.delete(0,END)
     RESULTAT.delete(0,END)
+    DIAGNOSTIC.delete(0,END)
 
 IMC_WIN=Tk()
 IMC_WIN.geometry("960x540")
@@ -46,26 +61,20 @@ resultat.place(x=100,y=220)
 RESULTAT=Entry(IMC_WIN,font=my_font,bg=bg)
 RESULTAT.place(x=450,y=220)
 
-
-
-
-
-
-
-
+diagnostic=Label(IMC_WIN,text="Diagnostic : ",font=my_font, bg=bg)
+diagnostic.place(x=100,y=290)
+DIAGNOSTIC=Entry(IMC_WIN,font=my_font,bg=bg)
+DIAGNOSTIC.place(x=450,y=290)
 
 
 
 validation=Button(IMC_WIN,text="Valider",font=my_font,bg=bg,command=go)
 fermer=Button(IMC_WIN,text="Quitter",font=my_font,command=IMC_WIN.destroy)
 reinit_=Button(IMC_WIN,text="Reinitialiser la fenêtre",font=my_font,command=reset)
-validation.place(x=100,y=290)
-fermer.place(x=100,y=350)
-reinit_.place(x=100,y=410)
 
-
-
-
+validation.pack(side=BOTTOM,padx=0,pady=0)
+fermer.pack(side=BOTTOM,padx=0,pady=0)
+reinit_.pack(side=BOTTOM,padx=0,pady=0)
 
 
 
